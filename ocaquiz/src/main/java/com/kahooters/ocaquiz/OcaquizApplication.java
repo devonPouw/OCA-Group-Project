83e3e7b38c6b026d3d9006760da681fe4921155e
avd-web <avd-web@outlook.com>
2023-07-17 13:24:26 +0200

    initial commit

---
+++ ocaquiz/src/main/java/com/kahooters/ocaquiz/OcaquizApplication.java
@@ -0,0 +1,13 @@
+package com.kahooters.ocaquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcaquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcaquizApplication.class, args);
+	}
+
+}
---
+++ ocaquiz/src/test/java/com/kahooters/ocaquiz/OcaquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kahooters.ocaquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcaquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
