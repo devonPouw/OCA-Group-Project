c7178d92a85fb02bcd4b19b283a692624baadecc
Cannon95 <104592959+Cannon95@users.noreply.github.com>
2023-07-19 12:40:44 +0200

    Testing GIT with a simple model class (#4)

---
+++ ocaquiz/src/main/java/com/kahooters/ocaquiz/Model/Quiz.java
@@ -0,0 +1,24 @@
+package com.kahooters.ocaquiz.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long ID;
+    public String name;
+
+}
