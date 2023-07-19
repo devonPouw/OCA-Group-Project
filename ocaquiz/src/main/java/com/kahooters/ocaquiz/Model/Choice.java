25fc5eecc91cea436f8cb3f8646d01e54e7fafa9
Cannon <markvdvliert@gmail.com>
2023-07-19 15:56:24 +0200

    Added the models and Repository files for the project.

---
+++ ocaquiz/src/main/java/com/kahooters/ocaquiz/Model/Choice.java
@@ -0,0 +1,29 @@
+package com.kahooters.ocaquiz.Model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Choice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long ID;
+
+    public String description;
+    public Integer isCorrect;
+
+    @ManyToOne
+    @JsonBackReference
+    public Question question;
+
+
+}
--- ocaquiz/src/main/java/com/kahooters/ocaquiz/Model/Tag.java
+++ ocaquiz/src/main/java/com/kahooters/ocaquiz/Model/Tag.java
@@ -1,7 +1,6 @@
 package com.kahooters.ocaquiz.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,24 +12,17 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
-public class Quiz {
+@NoArgsConstructor
+public class Tag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long ID;
     public String name;
 
-    @JoinTable(
-            name = "quizes_questions",
-            joinColumns = @JoinColumn(name = "question_id"),
-            inverseJoinColumns = @JoinColumn(name = "quiz_id")
-    )
+    @ManyToMany(mappedBy = "tags")
+    @JsonIgnore
     public List<Question> questions;
-
-    @OneToMany(mappedBy = "quiz")
-    @JsonManagedReference
-    public List<ChosenAnswer> chosenAnswers;
 
 }
